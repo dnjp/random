0a213674b01e8b0038caabc837a2e5ad01e241d9
Daniel Posthuma <dnjp@posteo.org>
2023-12-30 00:18:34 -0500

    feat: add uposatha date parser

---
+++ uposatha/README.md
@@ -0,0 +1,5 @@
+# Uposatha
+
+Uposatha days are days of observance in the Buddhist tradition.
+
+This script creates a Google Calendar CSV based on the poorly formatted dates copied from the [Metta Forest Monastery calendar](https://www.watmetta.org/calendar.html).
---
+++ uposatha/main.py
@@ -0,0 +1,36 @@
+"""
+This parses individual dates from csv
+"""
+
+import csv
+
+from datetime import date
+from datetime import datetime
+
+IN_FILE_PATH = "./dates.csv"
+OUT_FILE_PATH = "./out.csv"
+YEAR = 2024
+EVENT_DESCRIPTION = "Buddhist day of observance. Dates gathered from Metta Forest Monastery: https://www.watmetta.org/"
+
+all_dates = []
+with open(IN_FILE_PATH, mode="r", newline="", encoding="utf-8") as file:
+    r = csv.reader(file)
+    for row in r:
+        if not len(row) == 1:
+            continue
+
+        original_date = row[0]
+        items = original_date.split(" ")
+        month = datetime.strptime(items[0], "%B").month
+        days = [item.replace("*", "") for item in items[1:]]
+        dates = [date(YEAR, month, int(day)).strftime("%m/%d/%Y") for day in days]
+        all_dates.extend(dates)
+
+with open(OUT_FILE_PATH, mode="w", newline="", encoding="utf-8") as file:
+    w = csv.writer(file)
+    date_rows = [["Subject", "Start Date", "All Day Event", "Description"]]
+    for date in all_dates:
+        date_rows.append(["Uposatha", date, True, EVENT_DESCRIPTION])
+
+    for row in date_rows:
+        w.writerow(row)
